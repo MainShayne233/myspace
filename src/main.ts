fd7aeb957bf126caeb1bbc76f83e4b1ff22751eb
MainShayne233 <shaynetremblay@gmail.com>
2021-07-31 12:29:41 -0400

    Make final app

--- src/main.ts
+++ src/main.ts
@@ -1,67 +1,115 @@
-import Aqua, { Request, Response } from "https://deno.land/x/aqua/mod.ts";
 import ContentHandler from "https://deno.land/x/aqua@v1.1.4/content_handler.ts";
 
 const port = parseInt(Deno.env.get("PORT") || "4000");
+const host = Deno.env.get("HOST") || `localhost:${port}`;
+const httpHeader = Deno.env.get("HTTP_HEADER") || "http://";
 
-const app = new Aqua(port);
+const server = Deno.listen({ port });
 
-app.get("/", (_req) => {
-  return { redirect: "/home" };
-});
-
-function pageNamesRegEx() {
-  const pages = [];
-
-  for (const { name, isDirectory } of Deno.readDirSync("src/pages")) {
-    if (isDirectory) {
-      pages.push(name);
+for await (const conn of server) {
+  (async () => {
+    const httpConn = Deno.serveHttp(conn);
+    for await (const requestEvent of httpConn) {
+      hanldeRequest(requestEvent)
     }
-  }
-  return new RegExp("/(" + pages.join("|") + ")");
+  })();
 }
 
-app.get(pageNamesRegEx(), async (req) => {
-  const [page] = req.matches;
-  const path = "build/static/" + page + "/index.html";
-  return await app.render(path);
-});
-
-app.get(new RegExp("/(script|style).min\.(.*)\.(js|css)"), async (req) => {
-  const [type, key, ext] = req.matches;
-  const staticPath = staticPathForPage(req);
-  const path = staticPath + "/" + type + ".min." + key + "." + ext;
-  return await app.render(path);
-});
-
-app.get("/assets/:asset", async (req) => {
-  const staticPath = staticPathForPage(req);
-  const path = staticPath + "/assets/" + req.parameters.asset;
-  console.log(path)
-  const res = await app.render(path);
-  return res;
-});
-
-app.register((req, res) => {
-  const contentType = ContentHandler.getContentType(extension(req.url));
-  if (contentType) {
-    return { ...res, headers: { ...res.headers, "Content-Type": contentType } };
-  } else {
-    return res;
+function hanldeRequest(requestEvent: Deno.RequestEvent) {
+  if (removeHttpHeader(requestEvent.request.url).split("/")[0] === host) {
+    redirectTo(requestEvent, `home.${host}`);
+    return
   }
-});
+
+  const subDomain = getSubDomain(requestEvent);
+  const extension = getExtension(requestEvent);
+  const contentType = getContentType(extension) || "text/html";
+
+  if (contentType === "image/gif" || contentType === "image/x-icon") {
+    const fileName = getFileName(requestEvent);
+    const path = `build/static/${subDomain}/assets/${fileName}`
+    serveFile(requestEvent, path);
+  } else if (contentType === "text/html") {
+    const path = `build/static/${subDomain}/index.html`
+    serveFile(requestEvent, path)
+  } else if (contentType === "text/css" || contentType === "application/javascript") {
+    const fileName = getFileName(requestEvent);
+    const path = `build/static/${subDomain}/${fileName}`
+    serveFile(requestEvent, path);
+  } else {
+   throw new Error("Unimplemented")
+  }
+}
+
+function getContentType(extension: string) {
+  return ContentHandler.getContentType(extension);
+}
+
+function getSubDomain(requestEvent: Deno.RequestEvent) {
+  return removeHttpHeader(requestEvent.request.url).split(host)[0].split(".")[0]
+}
+
+function removeHttpHeader(url: string) {
+  return url.replace(new RegExp("https?://"), "");
+}
+
+function getExtension(requestEvent: Deno.RequestEvent) {
+  return extension(requestEvent.request.url);
+}
+
+function getFileName(requestEvent: Deno.RequestEvent) {
+  const url = requestEvent.request.url
+  const host = requestEvent.request.headers.get("host");
+
+  if (url === null || host === null) {
+    throw new Error("one of these is null: url: " + url + ", host: " + host);
+  }
+
+  return url.replace(new RegExp("https?://"), "").replace(host, "").split("/").slice(-1)[0];
+}
+
+function serveFile(requestEvent: Deno.RequestEvent, path: string) {
+  Deno.readFile(path).then((data) => {
+    requestEvent.respondWith(
+      new Response(data, {
+        status: 200,
+        headers: {
+          "Content-Type": contentType(path),
+        },
+      }),
+    );
+  }).catch((error) => {
+    requestEvent.respondWith(
+      new Response(error, {
+        status: 400,
+      }),
+    );
+  })
+}
+
+function redirectTo(requestEvent: Deno.RequestEvent, url: string) {
+  requestEvent.respondWith(
+    new Response(null, {
+      status: 301,
+      headers: {
+      location: httpHeader + url,
+      }
+    }),
+  );
+}
+
+function contentType(path: string) {
+  const contentType = ContentHandler.getContentType(extension(path));
+  if (contentType) {
+    return contentType;
+  } else {
+    throw new Error("Could not determine content type of: " + path);
+  }
+}
 
 function extension(path: string): string {
   return path.replace(
     /.*(?=\.[a-zA-Z0-9_]*$)/,
     "",
   );
-}
-
-function staticPathForPage(req: Request) {
-  console.log(req)
-  return "build/static" +
-    (req.headers.referer || "").replace(new RegExp("https?://"), "").replace(
-      req.headers.host,
-      "",
-    );
 }
